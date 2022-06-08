0c44a64b86107103813680276311a02cb4a1bfcb
Vincentsarsah <vincent.sarsah@amalitech.org>
2022-06-08 15:27:39 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const products = require('./products');
+const express = require("express");
+const cors = require("cors");
+const regions = require("./regions");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.get("/", (req, res)=> {
-    res.send("Welcome to our online shop API...")
+
+app.get("/", (req, res) => {
+  res.send(regions);
 });
 
-app.get('/products', (req, res)=> {
-    res.send(products)
-})
-
 const port = process.env.PORT || 5000;
-app.listen(port, console.log(`Server is runnung on port ${port}`))
-
+app.listen(port, console.log(`Server is runnung on port ${port}`));
